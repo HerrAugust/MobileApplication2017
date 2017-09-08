4b88fd49ff979f1d78a79a6280e8c783fbae57cb
Andrea Bianchi <and_bianchi@hotmail.it>
2017-09-08 10:53:48 +0200

    reverse

--- client/MyDogCare/src/constants.ts
+++ client/MyDogCare/src/constants.ts
@@ -1,6 +1,11 @@
-export const USE_PROXY = false;
+//export const USE_PROXY = false;
 
-export const URL_BASE = USE_PROXY ? 'api/' : 'http://192.168.1.4:8081/mydogcare/api/';
+//export const URL_BASE = USE_PROXY ? 'api/' : 'http://192.168.1.4:8081/mydogcare/api/';
+
+export const USE_PROXY = true;
+
+export const URL_BASE = USE_PROXY ? 'api/' : 'http://localhost:8081/mydogcare/api/';
+
 
 export const URL = {
     USERS: {
